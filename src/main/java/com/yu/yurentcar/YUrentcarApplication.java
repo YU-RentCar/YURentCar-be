530f32b26d639903709730448d998405436093c2
kangchanghan <anycar8515@ynu.ac.kr>
2023-05-01 18:35:25 +0900

    Chore : 프로젝트 생성
    - Java 17
    - springboot 3.0.2

---
+++ src/main/java/com/yu/yurentcar/YUrentcarApplication.java
@@ -0,0 +1,13 @@
+package com.yu.yurentcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YUrentcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YUrentcarApplication.class, args);
+    }
+
+}
