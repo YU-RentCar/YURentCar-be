42fdc41c19afc7a65dc1b3a434e8a3415828fe07
kangchanghan <anycar8515@ynu.ac.kr>
2023-05-24 19:18:10 +0900

    Feat : Card Entity 수정
    - Car Entity class
       - 타 엔티티 참조 필드명의 id 제거
          - user
       - 수정 불가 지정 : @Column(updatable = false)
          - cardNumber, CVC, expiredDate, registerDate
       - CVC 3자리로 제한 : @Length(min = 3, max = 3)
       - expiredDate 미래로 제한 : @Future
       - registerDate 현재 이하로 제한 : @PastOrPresent
    
    Related to : #1

--- src/main/java/com/yu/yurentcar/domain/reservation/entity/Card.java
+++ src/main/java/com/yu/yurentcar/domain/reservation/entity/Card.java
@@ -3,10 +3,13 @@ package com.yu.yurentcar.domain.reservation.entity;
 
 import com.yu.yurentcar.domain.user.entity.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDateTime;
 
@@ -24,21 +27,24 @@ public class Card {
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User userId;
+    private User user;
 
     @NotNull
-    @Column(name = "card_number",length = 20)
+    @Column(name = "card_number", length = 20, updatable = false)
     private String cardNumber;
 
     @NotNull
-    @Column(length = 3)
+    @Length(min = 3, max = 3)
+    @Column(length = 3, updatable = false)
     private String CVC;
 
     @NotNull
-    @Column(name = "expired_date")
+    @Future // 미래 시간
+    @Column(name = "expired_date", updatable = false)
     private LocalDateTime expiredDate;
 
     @NotNull
+    @PastOrPresent // 현재 또는 현재 이전
     @Column(name = "register_date")
     private LocalDateTime registerDate;
 }
