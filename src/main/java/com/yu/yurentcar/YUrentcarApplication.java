f107ae34a9d63f55790e86a60d20e165bbaf4144
Yeongjun <rnjs1522@naver.com>
2023-06-01 14:42:54 +0900

    Revert : 잘못된 커밋 복구

--- src/main/java/com/yu/yurentcar/YUrentcarApplication.java
+++ src/main/java/com/yu/yurentcar/YUrentcarApplication.java
@@ -2,13 +2,14 @@ package com.yu.yurentcar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@SpringBootApplication
 @EnableJpaAuditing
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class YUrentcarApplication {
 
-    public static void main(String[] args) {SpringApplication.run(YUrentcarApplication.class, args);}
+    public static void main(String[] args) {
+        SpringApplication.run(YUrentcarApplication.class, args);
+    }
 
 }
