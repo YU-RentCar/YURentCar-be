53bc535095b4e8045780d354163f2ce113cbb208
kangchanghan <anycar8515@ynu.ac.kr>
2023-06-02 20:51:09 +0900

    Feat : JwtFilter, TokenProvider, LoginSuccessHandler 구현 및 SecurityConfig에 적
     - JwtFilter
        - OncePerRequestFilter 상속
        - doFilterInternal
           - 받아온 request 로부터 jwt accessCookie 를 읽어서 TokenProvider 로 검증
           - 검증 결과를 SecurityContextHolder 에 담음
        - resolveToken
           - 쿠키로부터 accessToken 반환
    
     - TokenProvider util
        - generateTokenDto(token)
           - authentication 를 통해 토큰을 생성하여 반환
        - getAuthentication(accessToken)
           - token 을 통해 authentication 반환
        - validateToken(token)
           - token 유효성 검사
        - parseName(token)
           - token 에서 제목(subject) 추출
        - parseClaims(token)
           - token 에서 claims 추출
    
     - UserLoginSuccessHandler
        - 인증 정보를 읽어서 토큰을 생성
        - 토큰을 redis 에 저장
        - 쿠키 생성 및 response 에 담음
        - home 으로 redirect
    
     - SecurityConfig
        - 각 bean 객체 생성자에 필요한 인자들을 제공
        - 유효하지 않은 접근에 대한 에러 처리
           - authenticationEntryPoint
           - accessDeniedHandler
        - 세션 없애기
        - 접근권한 설정
            - 현재 설정은 구현된 메소드의 접근 모두 허용
        - 소셜 로그인 성공 후 처리 handler 설정
           - UserLoginSuccessHandler
        - 로그아웃 로직 추가
           - logout 접근 시 로그아웃
           - logoutHandler
              - 쿠키 삭제
              - SecurityContextHolder 삭제
              - 로그아웃 이후 200(OK) 반환
              - 헤더에서 읽어온 토큰에 대해 삭제 처리 -> 해당 코드는 주석(미사용)
    
    Related to : #36

--- src/main/java/com/yu/yurentcar/security/config/SecurityConfig.java
+++ src/main/java/com/yu/yurentcar/security/config/SecurityConfig.java
@@ -2,27 +2,47 @@ package com.yu.yurentcar.security.config;
 
 import com.yu.yurentcar.domain.user.repository.UserRepository;
 import com.yu.yurentcar.domain.user.service.CustomOAuth2UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.yu.yurentcar.security.filter.JwtFilter;
+import com.yu.yurentcar.security.handler.UserLoginSuccessHandler;
+import com.yu.yurentcar.security.service.TokenRedisService;
+import com.yu.yurentcar.utils.CookieProvider;
+import com.yu.yurentcar.utils.TokenProvider;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Value("${my.web.base-url}")
     private String webBaseUrl;
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    private final TokenRedisService tokenRedisService;
+
+    private final TokenProvider tokenProvider;
+    private final CookieProvider cookieProvider;
 
     public CustomOAuth2UserService oAuth2UserDetailsService() {
         return new CustomOAuth2UserService(userRepository, passwordEncoder());
+    }
+
+    public UserLoginSuccessHandler userLoginSuccessHandler() {
+        return new UserLoginSuccessHandler(tokenProvider, cookieProvider, tokenRedisService, webBaseUrl);
     }
 
 
@@ -34,17 +54,68 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
 
-        http.cors().and().csrf().disable();
+        http.cors().and().csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> {
+                    // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                })
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    // 필요한 권한 없이 접근하려 할때 403
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                })
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        http.authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/api/v1/**").permitAll()
+                        .anyRequest().denyAll());
+
 
         //로컬 로그인
         //http.formLogin();
 
         //소셜 로그인
         http.oauth2Login()
-                .defaultSuccessUrl(webBaseUrl)
-                //.successHandler()
+                //.defaultSuccessUrl(webBaseUrl)
                 .userInfoEndpoint()
-                .userService(oAuth2UserDetailsService());
+                .userService(oAuth2UserDetailsService())
+                .and()
+                .successHandler(userLoginSuccessHandler());
+
+        http.logout()
+                //.logoutSuccessHandler()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .addLogoutHandler((request, response, authentication) -> {
+
+                    Cookie refreshToken = new Cookie("refreshToken", null);
+                    refreshToken.setPath("/");
+                    refreshToken.setHttpOnly(true);
+                    refreshToken.setMaxAge(0);
+                    refreshToken.setDomain(DOMAIN);
+                    Cookie accessToken = new Cookie("accessToken", null);
+                    accessToken.setPath("/");
+                    accessToken.setHttpOnly(true);
+                    accessToken.setMaxAge(0);
+                    accessToken.setDomain(DOMAIN);
+
+                    /*
+                    String tokenString = request.getHeader("Authorization");
+                    try {
+                        tokenRedisService.deleteToken(tokenString);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                     */
+                    SecurityContextHolder.clearContext();
+                })
+                .deleteCookies()
+                .invalidateHttpSession(true)
+                .logoutSuccessHandler((request, response, authentication) -> response.setStatus(HttpServletResponse.SC_OK));
+
 
 
         return http.build();
