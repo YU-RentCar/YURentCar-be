5965cbfea63b37d9acbd1c2711f0cf1e57d45c0e
kangchanghan <anycar8515@ynu.ac.kr>
2023-06-03 20:50:03 +0900

    Fix : ChangeNicknameDto의 요소 수정
     - ChangeNicknameDto
        - @NoArgsConstructor 추가
           - JSON parse를 위해 기본생성자를 추가해줌
    
    Related to : #27

--- src/main/java/com/yu/yurentcar/domain/user/dto/ChangeNicknameDto.java
+++ src/main/java/com/yu/yurentcar/domain/user/dto/ChangeNicknameDto.java
@@ -2,10 +2,12 @@ package com.yu.yurentcar.domain.user.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
+@NoArgsConstructor
 public class ChangeNicknameDto {
     private String nickname;
 
