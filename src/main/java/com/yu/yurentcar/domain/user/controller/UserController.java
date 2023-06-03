efb4db5add596eea58ad3c7990d464cc2ada7c3d
kangchanghan <anycar8515@ynu.ac.kr>
2023-06-03 16:17:44 +0900

    Feat : User Entity 인덱스 추가, DynamicUpdate 수정 및 UserController 함수명 변경
     - User Entity class
        - @AllArgs, @Equals, @Builder 제거
           - @Builder.Default 제거
        - userId 제외한 생성자 및 Builder 추가
        - Update method 실행 시 Null 여부 체크
    
     - UserController
        - 생성자 @RequiredArgsConstructor 로 변경
        - 함수명 lookup -> get 변경
        - 경로명 수정
           - 변경에 대한 경로명 수정은 /change 제거
        - 변경 메소드에 대한 반환값 Boolean 변경
        - 사용자 정보 SecurityContext 로부터 호출
           - @CurrentSecurityContext(expression = "authentication.principal") UserAuthDto auth
    
    Related to : #1, #27, #31, #50

--- src/main/java/com/yu/yurentcar/domain/user/controller/UserController.java
+++ src/main/java/com/yu/yurentcar/domain/user/controller/UserController.java
@@ -2,62 +2,66 @@ package com.yu.yurentcar.domain.user.controller;
 
 import com.yu.yurentcar.domain.user.dto.ChangeNicknameDto;
 import com.yu.yurentcar.domain.user.dto.PreferFilterDto;
+import com.yu.yurentcar.domain.user.dto.UserAuthDto;
 import com.yu.yurentcar.domain.user.dto.UserProfileDto;
-import com.yu.yurentcar.domain.user.entity.User;
 import com.yu.yurentcar.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1/users")
 public class UserController {
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
+    // no auth
     @GetMapping(value = "/nicknames/exists")
     public ResponseEntity<Boolean> checkNicknameDuplicate(@RequestParam String nickname) {
-        log.info("nickname : " + nickname);
+        log.info("new nickname : " + nickname);
         return ResponseEntity.status(HttpStatus.OK).body(userService.checkNicknameDuplicate(nickname));
     }
 
-    @PatchMapping(value = "/preferfilter")
-    public ResponseEntity<User> changePreferFilter(@RequestParam String nickname,@RequestBody PreferFilterDto preferFilterDto) {
-        log.info("data : " + preferFilterDto.toString());
-        return ResponseEntity.status(HttpStatus.OK).body(userService.changePreferFilter(nickname,preferFilterDto));
+    @GetMapping(value = "/prefer-filter")
+    public ResponseEntity<PreferFilterDto> getPreferFilter(@CurrentSecurityContext(expression = "authentication.principal") UserAuthDto auth) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getPreferFilter(auth.getNickname()));
     }
-    @GetMapping(value = "/preferfilter")
-    public ResponseEntity<PreferFilterDto> lookupPreferFilter(@RequestParam String nickname) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.lookupPreferFilter(nickname));
+
+    @PatchMapping(value = "/prefer-filter")
+    public ResponseEntity<Boolean> changePreferFilter(@RequestBody PreferFilterDto preferFilterDto, @CurrentSecurityContext(expression = "authentication.principal") UserAuthDto auth) {
+        log.info("data : " + preferFilterDto);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                userService.changePreferFilter(auth.getUsername(),preferFilterDto)
+        );
     }
 
     @PatchMapping(value = "/nicknames")
-    public ResponseEntity<User> changeNickname(@RequestBody ChangeNicknameDto changeNicknameDto) {
-        log.info("Username : " + changeNicknameDto.getUsername() + " nickname : " + changeNicknameDto.getNickname());
-        return ResponseEntity.status(HttpStatus.OK).body(userService.changeNickname(changeNicknameDto));
+    public ResponseEntity<Boolean> changeNickname(@RequestBody ChangeNicknameDto changeNicknameDto,
+                                               @CurrentSecurityContext(expression = "authentication.principal") UserAuthDto auth) {
+        log.info("Username : " + auth.getUsername() + " nickname : " + changeNicknameDto.getNickname());
+        return ResponseEntity.status(HttpStatus.OK).body(userService.changeNickname(auth.getUsername(), changeNicknameDto.getNickname()));
     }
 
-    @PatchMapping(value = "/profiles/change")
-    public ResponseEntity<User> changeUserProfile(@RequestBody UserProfileDto userProfileDto) {
-        log.info(userProfileDto.toString());
-        return ResponseEntity.status(HttpStatus.OK).body(userService.changeProfile(userProfileDto));
-    }
+    // TODO : 추후 논의 후 추가
+//    @PatchMapping(value = "/profiles")
+//    public ResponseEntity<User> changeUserProfile(@RequestBody UserProfileDto userProfileDto) {
+//        log.info("user profiles change : " + userProfileDto.toString());
+//        return ResponseEntity.status(HttpStatus.OK).body(userService.changeProfile(userProfileDto));
+//    }
 
     @GetMapping(value = "/point")
-    public ResponseEntity<Integer> lookupPoint(@RequestParam String nickname) {
-        log.info("nickname : " + nickname);
-        return ResponseEntity.status(HttpStatus.OK).body(userService.lookupPoint(nickname));
+    public ResponseEntity<Integer> getPoint(@CurrentSecurityContext(expression = "authentication.principal") UserAuthDto auth) {
+        log.info("username : " + auth.getUsername());
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getPoint(auth.getUsername()));
     }
 
     @GetMapping(value = "/profiles")
-    public ResponseEntity<UserProfileDto> lookupUserProfile(@RequestParam String username){
-        return ResponseEntity.status(HttpStatus.OK).body(userService.lookupUserProfile(username));
+    public ResponseEntity<UserProfileDto> getUserProfile(@CurrentSecurityContext(expression = "authentication.principal") UserAuthDto auth){
+        log.info("username : " + auth.getUsername());
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUserProfile(auth.getUsername()));
     }
 }
