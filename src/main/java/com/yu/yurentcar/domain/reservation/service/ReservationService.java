c6036392086fa4b557a2bfb50374e121bcc53ae9
Yeongjun <rnjs1522@naver.com>
2023-06-05 16:45:48 +0900

    Feat : 예약 관련 API 개발
    - ReservationController
        - GET /api/v1/reservations/users/accidents
            - request : UserAuthDto auth
            - response : List<String>
        - GET /api/v1/reservations/users/repairs
            - request : UserAuthDto auth
            - response : List<String>
        - GET /api/v1/reservations/users/prices
            - request : UserAuthDto auth
            - response : Integer
        - GET /api/v1/reservations/users/cars
            - request : UserAuthDto auth
            - response : CarResponseDto
        - GET /api/v1/reservations/users/car-specs
            - request : UserAuthDto auth
            - response : CarSpecDto
        - GET /api/v1/reservations/users/branches/points
            - request : UserAuthDto auth
            - response : Point
        - GET /api/v1/reservations/users/details
            - request : UserAuthDto auth
            - response : ReservationDetailDto
    
    - ReservationDetailDto
        - 예약 상세 조회 DTO
    
    - ReservationRepository
        - JPA 및 NoticeRepositoryCustom 상속
    
    - ReservationRepositoryCustom
        - getAccidentListByUsername
            - Username에 해당하는 사용자의 렌트 대기중인 차량의 사고내역 리스트 반환
        - getRepairListByUsername
            - Username에 해당하는 사용자의 렌트 대기중인 차량의 수리내역 리스트 반환
        - getPriceByUsername
            - Username에 해당하는 사용자의 렌트 대기중인 예약의 가격 반환
        - getCarDtoByUsername
            - Username에 해당하는 사용자의 렌트 대기중인 차량의 정보 반환
        - getCarSpecificationDtoByUsername
            - Username에 해당하는 사용자의 렌트 대기중인 차량의 제원 반환
        - getBranchPointByUsername
            - Username에 해당하는 사용자의 렌트 대기중인 지점의 위도와 경도 반환
        - findNowReservationDetailByUsername
            - Username에 해당하는 사용자의 렌트 대기중인 예약의 상세 정보 반환
    
    - ReservationRepositoryImpl
        - getAccidentListByUsername
            - username으로 렌트 대기중인 예약을 조회
            - 렌트 대기중인 예약의 차량 조회 후 사고내역 반환
        - getRepairListByUsername
            - username으로 렌트 대기중인 예약을 조회
            - 렌트 대기중인 예약의 차량 조회 후 수리내역 반환
        - getPriceByUsername
            - username으로 렌트 대기중인 예약을 조회 후 가격 반환
        - getCarDtoByUsername
            - username으로 렌트 대기중인 예약을 조회
            - 렌트 대기중인 예약의 차량 조회 후 차 정보 반환
        - getCarSpecificationDtoByUsername
            - username으로 렌트 대기중인 예약을 조회
            - 렌트 대기중인 예약의 차량 제원 조회 후 차량 제원 반환
        - getBranchPointByUsername
            - username으로 렌트 대기중인 예약을 조회
            - 렌트 대기중인 예약의 지점 정보 조회 후 위도와 경도 반환
        - findNowReservationDetailByUsername
            - username으로 렌트 대기중인 예약을 조회
            - 필요한 테이블을 innerjoin 후 ReservationDetailDto로 반환
        - findLatestReservationInfo
            - username으로 렌트 대기중인 예약의 JPAQuery문 반환
    
    - ReservationService
        - getAccidentListByUsername
        - getRepairListByUsername
        - getPriceByUsername
        - getCarByUsername
        - getCarSpecByUsername
        - getBranchPointByUsername
        - getNowReservationDetailByUsername
    
    - CarResponseDto
        - 차량 정보 DTO
    
    - CarSpecDto
        - 차량 제원 DTO
    
    Related to : #61

---
+++ src/main/java/com/yu/yurentcar/domain/reservation/service/ReservationService.java
@@ -0,0 +1,51 @@
+package com.yu.yurentcar.domain.reservation.service;
+
+import com.yu.yurentcar.domain.car.dto.CarResponseDto;
+import com.yu.yurentcar.domain.car.dto.CarSpecDto;
+import com.yu.yurentcar.domain.reservation.dto.ReservationDetailDto;
+import com.yu.yurentcar.domain.reservation.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.geo.Point;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ReservationService {
+    private final ReservationRepository reservationRepository;
+
+    @Transactional(readOnly = true)
+    public List<String> getAccidentListByUsername(String username){
+        return reservationRepository.getAccidentListByUsername(username);
+    }
+    @Transactional(readOnly = true)
+    public List<String> getRepairListByUsername(String username){
+        return reservationRepository.getRepairListByUsername(username);
+    }
+    @Transactional(readOnly = true)
+    public Integer getPriceByUsername(String username){
+        return reservationRepository.getPriceByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public CarResponseDto getCarByUsername(String username){
+        return reservationRepository.getCarDtoByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public CarSpecDto getCarSpecByUsername(String username){
+        return reservationRepository.getCarSpecificationDtoByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Point getBranchPointByUsername(String username){
+        return reservationRepository.getBranchPointByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public ReservationDetailDto getNowReservationDetailByUsername(String username) {
+        return reservationRepository.findNowReservationDetailByUsername(username);
+    }
+}
