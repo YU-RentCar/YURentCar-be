fd340282fb5239b4a37c1e91faa57417711f550b
kangchanghan <anycar8515@ynu.ac.kr>
2023-06-06 09:18:22 +0900

    Feat : Branch 위도/경도 반환하는 API 개발
     - BranchController
        - GET /api/v1/branches/location
        - request : province, branchName
        - response : x(위도), y(경도)
    
     - BranchRepositoryCustom
        - Point getGeoPointByBranchName(SiDoType siDo, String branchName) 추가
    
     - BranchRepositoryImpl
        - 시/도, 지점명이 일치하는 지점의 위도, 경도 반환
    
     - BranchService
        - 입력받은 province를 SiDoType으로 변환 후 repository에 요청
    
    Usages : #24
    Resolved : #63

--- src/main/java/com/yu/yurentcar/domain/branch/controller/BranchController.java
+++ src/main/java/com/yu/yurentcar/domain/branch/controller/BranchController.java
@@ -1,7 +1,9 @@
 package com.yu.yurentcar.domain.branch.controller;
 
 import com.yu.yurentcar.domain.branch.service.BranchService;
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.geo.Point;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,10 @@ public class BranchController {
         log.info("===BranchController===");
         log.info("getBranchList : " + siDo);
         return ResponseEntity.ok(branchService.getBranchNameList(siDo));
+    }
+
+    @GetMapping("location")
+    public ResponseEntity<Point> getBranchGeoPoint(@RequestParam @Valid String province, @RequestParam @Valid String branchName) {
+        return ResponseEntity.ok(branchService.getGeoPoint(province, branchName));
     }
 }
