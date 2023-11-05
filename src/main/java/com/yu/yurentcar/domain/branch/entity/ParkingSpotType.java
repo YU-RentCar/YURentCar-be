b59c5622c9acbd52b2b31ebb085835fb10bc93a2
Yeongjun <rnjs1522@naver.com>
2023-11-05 10:20:01 +0900

    Fix : 주차장 상태 추가
    - ParkingSpotType
        - 주차 가능의 상태 분할
    
    Related to : #115

--- src/main/java/com/yu/yurentcar/domain/branch/entity/ParkingSpotType.java
+++ src/main/java/com/yu/yurentcar/domain/branch/entity/ParkingSpotType.java
@@ -9,8 +9,9 @@ import lombok.RequiredArgsConstructor;
 public enum ParkingSpotType implements EnumValue<Integer> {
     SIDEWALK("인도", 1),
     DRIVEWAY("차도",2),
-    PARKINGSPOT_POSSIBLE("주차 가능",3),
-    PARKINGSPOT_IMPOSSIBLE("주차 불가능",4);
+    PARKINGSPOT_CAR("주차 중",3),
+    PARKINGSPOT_NO_CAR("주차 가능",4),
+    PARKINGSPOT_IMPOSSIBLE("주차 불가능",5);
 
     private final String desc;
     private final Integer dbValue;
