2b7159ed7f09a635154081c6e890c82f4d099afc
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-11 23:30:45 +0900

    Fix: 특정 예약에서 변경 가능한 차량 리스트를 가져오는 기능 수정
    - AdminRepositoryImpl
      - isReservationByAdminBranch에서 반대로 반환하고 있던점 수정
    
    - BranchService
      - carRepository.findUpdatableCarListByDateAndReservationId를 제거하고 기존 로직으로 대체
      - carService.getCarListByAdmin
      - carRepository.updatableByCarNumberAndDate로 필터링
    
    Related to : #107

--- src/main/java/com/yu/yurentcar/domain/branch/service/BranchService.java
+++ src/main/java/com/yu/yurentcar/domain/branch/service/BranchService.java
@@ -3,7 +3,9 @@ package com.yu.yurentcar.domain.branch.service;
 import com.yu.yurentcar.domain.branch.dto.UpdatableCarListRequestParamsDto;
 import com.yu.yurentcar.domain.branch.dto.UpdatableCarResponseDto;
 import com.yu.yurentcar.domain.branch.repository.BranchRepository;
+import com.yu.yurentcar.domain.car.dto.CarManagementDto;
 import com.yu.yurentcar.domain.car.repository.CarRepository;
+import com.yu.yurentcar.domain.car.service.CarService;
 import com.yu.yurentcar.domain.user.repository.AdminRepository;
 import com.yu.yurentcar.global.SiDoType;
 import com.yu.yurentcar.global.utils.enums.EnumValueConvertUtils;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
@@ -20,11 +23,13 @@ public class BranchService {
     private final CarRepository carRepository;
     private final BranchRepository branchRepository;
     private final AdminRepository adminRepository;
+    private final CarService carService;
 
-    public BranchService(CarRepository carRepository, BranchRepository branchRepository, AdminRepository adminRepository) {
+    public BranchService(CarRepository carRepository, BranchRepository branchRepository, AdminRepository adminRepository, CarService carService) {
         this.carRepository = carRepository;
         this.branchRepository = branchRepository;
         this.adminRepository = adminRepository;
+        this.carService = carService;
     }
 
     @Transactional(readOnly = true)
@@ -50,6 +55,17 @@ public class BranchService {
         if (!isReservationInAdminBranch)
             throw new RuntimeException("해당 관리자가 없거나 해당 관리자 지점의 예약이 아닙니다.");
 
-        return carRepository.findUpdatableCarListByDateAndReservationId(requestDto.getStartDate(), requestDto.getEndDate(), requestDto.getReservationId());
+        List<CarManagementDto> carNumberListInReservationBranch = carService.getCarListByAdmin(requestDto.getAdminUsername());
+        return carNumberListInReservationBranch.stream()
+                .filter((car) ->
+                        carRepository.updatableByCarNumberAndDate(
+                                requestDto.getReservationId(),
+                                car.getCarNumber(),
+                                requestDto.getStartDate(),
+                                requestDto.getEndDate()
+                        )
+                )
+                .map((car) -> new UpdatableCarResponseDto(car.getCarName(), car.getCarNumber()))
+                .collect(Collectors.toList());
     }
 }
